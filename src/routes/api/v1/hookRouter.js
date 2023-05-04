75ce342dfb3dfc76e57706fa24d12637a9d4a220
jacoobanderson <j00cwa@gmail.com>
2023-05-04 20:35:39 +0200

    Create webhook route

---
+++ src/routes/api/v1/hookRouter.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import { HookController } from '../../../controllers/HookController.js'
+import jwt from 'jsonwebtoken'
+import createError from 'http-errors'
+
+export const router = express.Router()
+
+const controller = new HookController()
+
+/**
+ * Authenticates the jwt.
+ *
+ * @param {object} req - Express request object.
+ * @param {object} res - Express response object.
+ * @param {Function} next - Express next middleware function.
+ */
+const authenticateJWT = (req, res, next) => {
+  const auth = req.headers.authorization?.split(' ')
+
+  if (auth?.[0] !== 'Bearer') {
+    next(createError(401))
+    return
+  }
+
+  try {
+    const payload = jwt.verify(auth[1], process.env.PUBLIC_KEY)
+    req.user = {
+      username: payload.sub,
+      id: payload.id
+    }
+    next()
+  } catch (error) {
+    const err = createError(401)
+    err.cause = error
+    next(err)
+  }
+}
+
+router.post('/', authenticateJWT, (req, res, next) => controller.addHook(req, res, next))
--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { router as userRouter } from './userRouter.js'
 import { router as catchRouter } from './catchRouter.js'
+import { router as hookRouter } from './hookRouter.js'
+
 export const router = express.Router()
 
 router.get('/', (req, res) => res.json({ message: 'Hooray! Welcome to version 1 of this very simple RESTful API!' }))
 router.use('/users', userRouter)
 router.use('/catches', catchRouter)
+router.use('/webhooks', hookRouter)
