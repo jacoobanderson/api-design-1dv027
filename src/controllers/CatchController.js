c9c8a02ebad67b87fe3d9248f2ba4e79e11ead78
jacoobanderson <j00cwa@gmail.com>
2023-05-04 19:28:56 +0200

    Create put functionality to update catch data

--- src/controllers/CatchController.js
+++ src/controllers/CatchController.js
@@ -13,7 +13,6 @@ export class CatchController {
    */
   async getAllCatches (req, res, next) {
     try {
-        console.log('test')
       const catches = await Catch.find({})
       res.status(200).json(catches)
     } catch (error) {
@@ -43,6 +42,70 @@ export class CatchController {
       await newCatch.save()
 
       res.status(201).json(catchData)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  async getSingleCatch (req, res, next) {
+    try {
+
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  async updateCatch (req, res, next) {
+    try {
+        console.log('test')
+      const id = req.params.id
+      console.log(id)
+      const catchToUpdate = await Catch.findById(id)
+
+      const newCatchData = {
+        username: req.user.username,
+        position: req.body.position,
+        lake: req.body.lake,
+        city: req.body.city,
+        species: req.body.species,
+        weight: req.body.weight,
+        length: req.body.length,
+        imageUrl: req.body.imageUrl
+      }
+      for (const [key, value] of Object.entries(newCatchData)) {
+        if (value !== undefined) {
+          catchToUpdate[key] = value
+        }
+      }
+
+      await catchToUpdate.save()
+      res.status(200).json(catchToUpdate)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  async deleteCatch (req, res, next) {
+    try {
+
     } catch (error) {
       next(error)
     }
