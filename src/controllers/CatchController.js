bf9e80c6d4d6615ccd11050fe8f9435f8f54941d
jacoobanderson <j00cwa@gmail.com>
2023-05-04 18:38:37 +0200

    Add create and findall functionality for catches

--- src/controllers/CatchController.js
+++ src/controllers/CatchController.js
@@ -1,8 +1,50 @@
-// import createError from 'http-errors'
-// import { Catch } from '../models/Catch.js'
+import createError from 'http-errors'
+import { Catch } from '../models/Catch.js'
 
 /**
  * Encapsulates a controller.
  */
 export class CatchController {
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  async getAllCatches (req, res, next) {
+    try {
+        console.log('test')
+      const catches = await Catch.find({})
+      res.status(200).json(catches)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  /**
+   *
+   * @param req
+   * @param res
+   * @param next
+   */
+  async createCatch (req, res, next) {
+    try {
+      const catchData = {
+        username: req.user.username,
+        position: req.body.position,
+        lake: req.body.lake,
+        city: req.body.city,
+        species: req.body.species,
+        weight: req.body.weight,
+        length: req.body.length,
+        imageUrl: req.body.imageUrl
+      }
+      const newCatch = new Catch(catchData)
+      await newCatch.save()
+
+      res.status(201).json(catchData)
+    } catch (error) {
+      next(error)
+    }
+  }
 }
